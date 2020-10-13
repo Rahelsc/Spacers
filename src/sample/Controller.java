de38b97a200650df60b9ab43eedacb20a2b27902
Rachel Schwartz <rahelsc@gmail.com>
2020-10-13 06:44:37 +0300

    last known changes

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,11 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.input.KeyEvent;
+
 public class Controller {
+    @FXML
+    public void write(KeyEvent k){
+        System.out.println("yoad yoad yoad!~!!");
+    }
 }
