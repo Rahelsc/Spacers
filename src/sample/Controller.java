f89b451c87365cc6b6b13a3e06e32181cbb5705f
Rachel Schwartz <rahelsc@gmail.com>
2020-10-14 18:53:10 +0300

    movement after shot fixed!

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,11 +1,14 @@
 package sample;
 
+import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
@@ -29,31 +32,37 @@ public class Controller {
 
     @FXML
     ImageView SpaceShip;
-    double imgX,imgY = 0;
+    double imgX, imgY = 0;
+
+    Timeline act;
 
     private ObservableList<KeyCode> keys = FXCollections.observableArrayList();
 
-    public void Fire(){
-        fired=true;
-        Timeline tm = new Timeline(new KeyFrame(Duration.millis(30),this::shotStep));
+    public void Fire() {
+        fired = true;
+        Timeline tm = new Timeline(new KeyFrame(Duration.millis(30), this::shotStep));
         tm.setCycleCount(Timeline.INDEFINITE);
         tm.play();
-        if(Border.getChildren().size()<2){
+        if (Border.getChildren().size() < 2) {
             tm.stop();
-            fired=false;
+            fired = false;
         }
     }
 
-    public void shotStep(ActionEvent e){
-        for (int i =1 ; i<Border.getChildren().size();i++) {
+    public void shotStep(ActionEvent e) {
+        for (int i = 1; i < Border.getChildren().size(); i++) {
             Border.getChildren().get(i).setTranslateY(Border.getChildren().get(i).getTranslateY() - 5);
-            if(Border.getChildren().get(i).getTranslateY()<=-300){
+            if (Border.getChildren().get(i).getTranslateY() <= -300) {
                 Border.getChildren().remove(Border.getChildren().get(i));
             }
         }
     }
 
-
+    @FXML
+    public void removeKey(KeyEvent event) {
+        System.out.println("YYYYYYYYY");
+        keys.remove(event.getCode());
+    }
 
     @FXML
     public void ManageMovment(KeyEvent e) throws InterruptedException {
@@ -61,45 +70,43 @@ public class Controller {
             keys.add(e.getCode());
         }
         Border.setOnKeyReleased((event) -> {
-            keys.remove(event.getCode());
+            removeKey(event);
         });
         System.out.println(keys);
         //moving smoothie with timeline (20 per 150 milis)
         //nasty conditions for set borders of the screen
-        Timeline act = new Timeline(new KeyFrame(Duration.millis(50),(somth)->{
-              if (keys.contains(KeyCode.LEFT)&&keys.contains(KeyCode.UP)) {
-                  if(imgX>-450)
-                      imgX-=1;
-                  if(imgY>-250)
-                      imgY-=1;
-            }
-            else if (keys.contains(KeyCode.RIGHT)&&keys.contains(KeyCode.UP)) {
-                  if(imgX<450)
-                      imgX+=1;
-                  if(imgY>-250)
-                      imgY-=1;
-            } else if (keys.contains(KeyCode.RIGHT)&&keys.contains(KeyCode.DOWN)) {
-                  if(imgY<250)
-                      imgY+=1;
-                  if(imgX<450)
-                      imgX+=1;
-            } else if (keys.contains(KeyCode.LEFT)&&keys.contains(KeyCode.DOWN)) {
-                  if(imgY<250)
-                      imgY+=1;
-                  if(imgX>-450)
-                      imgX-=1;
-            }
-            else if (keys.contains(KeyCode.RIGHT) && imgX<450)
-                      imgX+=1;
-            else if (keys.contains(KeyCode.LEFT) && imgX>-450)
-                      imgX-=1;
-            else if (keys.contains(KeyCode.UP) && imgY>-250)
-                      imgY-=1;
-            else if (keys.contains(KeyCode.DOWN) && imgY<250)
-                      imgY+=1;
+        act = new Timeline(new KeyFrame(Duration.millis(50), (somth) -> {
+            if (keys.contains(KeyCode.LEFT) && keys.contains(KeyCode.UP)) {
+                if (imgX > -450)
+                    imgX -= 1;
+                if (imgY > -250)
+                    imgY -= 1;
+            } else if (keys.contains(KeyCode.RIGHT) && keys.contains(KeyCode.UP)) {
+                if (imgX < 450)
+                    imgX += 1;
+                if (imgY > -250)
+                    imgY -= 1;
+            } else if (keys.contains(KeyCode.RIGHT) && keys.contains(KeyCode.DOWN)) {
+                if (imgY < 250)
+                    imgY += 1;
+                if (imgX < 450)
+                    imgX += 1;
+            } else if (keys.contains(KeyCode.LEFT) && keys.contains(KeyCode.DOWN)) {
+                if (imgY < 250)
+                    imgY += 1;
+                if (imgX > -450)
+                    imgX -= 1;
+            } else if (keys.contains(KeyCode.RIGHT) && imgX < 450)
+                imgX += 1;
+            else if (keys.contains(KeyCode.LEFT) && imgX > -450)
+                imgX -= 1;
+            else if (keys.contains(KeyCode.UP) && imgY > -250)
+                imgY -= 1;
+            else if (keys.contains(KeyCode.DOWN) && imgY < 250)
+                imgY += 1;
             SpaceShip.setTranslateX(imgX);
             SpaceShip.setTranslateY(imgY);
-
+            act.playFromStart();
         }));
         act.setCycleCount(25);
         act.play();
@@ -108,7 +115,7 @@ public class Controller {
         if (keys.contains(KeyCode.SPACE)) {
             Timer timer = new java.util.Timer();
             //delay the shooting by 300 milli sec
-            if(allowShooting) {
+            if (allowShooting) {
                 timer.schedule(new TimerTask() {
                     public void run() {
                         //nasty anonymous class cause of javaFX issue with Timer
@@ -119,13 +126,13 @@ public class Controller {
                                     Fire();
                                 Border.getChildren().add(shot.getC());
                                 shot.updatePosition(imgX + 47, imgY - 50);
-                                allowShooting=true;
+                                allowShooting = true;
                             }
                         });
                     }
                 }, 300);
             }
-            allowShooting=false;
+            allowShooting = false;
         }
 
     }
